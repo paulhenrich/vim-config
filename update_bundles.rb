602cf21b79e7df522fd32de53fa9579c35878a7a
paul <paul.henrich@gmail.com>
2011-06-22 23:29:15 -0400

    added coffeescript plugin

--- update_bundles.rb
+++ update_bundles.rb
@@ -45,6 +45,7 @@ def git_sub
     "git://github.com/davidoc/taskpaper.vim.git",
     "git://github.com/vim-scripts/jade.vim.git",
     "git://github.com/tpope/vim-haml.git",
+    "https://github.com/kchmck/vim-coffee-script.git",
   ]
 
   vim_dir     = File.dirname(__FILE__)
